09278ae9164ff59ca3d22ac3271f4990650bcd76
taayls <tayls@protonmail.com>
2020-04-20 15:13:38 +1200

    add server config

---
+++ server/index.js
@@ -0,0 +1,19 @@
+let config;
+
+try {
+  config = require('./config/server.config');
+} catch (e) {
+  console.log(e);
+  throw new Error('Missing server configuration file.');
+}
+
+const Logger = require('logplease');
+const logger = Logger.create('Server');
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app)
+
+http.listen(config.port);
+
+logger.info(`is now listing at: http://hydrobuddy.local:${config.port}`);
