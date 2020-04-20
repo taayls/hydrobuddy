6211105db03b3d3e28088290c331cd652852c9a9
taayls <tayls@protonmail.com>
2020-04-20 17:25:41 +1200

    add https support

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
   port: 3000,
-}
+  ssl_enabled: false,
+  ssl_port: 4000,
+  ssl_key_path: path.resolve('~/hydrobuddy.local.key'),
+  ssl_cert_path: path.resolve('~/hydrobuddy.local.crt'),
+};
--- server/index.js
+++ server/index.js
@@ -8,11 +8,37 @@ try {
 }
 
 const Logger = require('logplease');
+
 const logger = Logger.create('Server');
 
+const fs = require('fs');
+
 const express = require('express');
+
 const app = express();
+
 const http = require('http').createServer(app);
+
+const https = config.ssl
+  ? require('https').createServer(
+      {
+        key: fs.readFileSync(config.ssl_key_path),
+        cert: fs.readFileSync(config.ssl_cert_path),
+      },
+      app
+    )
+  : null;
+
+function redirectHTTPS(req, res, next) {
+  if (req.secure) {
+    return next();
+  }
+
+  logger.info('Redirecting HTTP connection to HTTPS.');
+  res.redirect('https://' + req.hostname + req.url);
+}
+
+if (config.ssl_enabled) app.all('*', redirectHTTPS);
 
 http.listen(config.port);
 
