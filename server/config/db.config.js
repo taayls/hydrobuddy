91bdf0b76184dd21da9aac3b9d85114114a0bd8e
taayls <tayls@protonmail.com>
2020-04-20 19:32:02 +1200

    setup database config

---
+++ server/config/db.config.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = {};
+
+const sequelize = new Sequelize({
+  username: 'root',
+  password: '',
+  database: 'hydrobuddy',
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
