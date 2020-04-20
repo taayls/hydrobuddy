7dd713edde76c6abed95f38faf5f1346b557d490
taayls <tayls@protonmail.com>
2020-04-20 20:51:18 +1200

    add purge function to motors

--- server/controllers/motors.js
+++ server/controllers/motors.js
@@ -19,6 +19,34 @@ const motors = {
 
     logger.info('Motors Initialized.');
   },
+
+  run_all: function () {
+    motorHatOne.dcs[0].runSync('fwd');
+    motorHatOne.dcs[1].runSync('fwd');
+    motorHatOne.dcs[2].runSync('fwd');
+    motorHatOne.dcs[3].runSync('fwd');
+    motorHatTwo.dcs[0].runSync('fwd');
+    motorHatTwo.dcs[1].runSync('fwd');
+    motorHatTwo.dcs[2].runSync('fwd');
+    motorHatTwo.dcs[3].runSync('fwd');
+  },
+
+  stop_all: function () {
+    motorHatOne.dcs[0].stopSync();
+    motorHatOne.dcs[1].stopSync();
+    motorHatOne.dcs[2].stopSync();
+    motorHatOne.dcs[3].stopSync();
+    motorHatTwo.dcs[0].stopSync();
+    motorHatTwo.dcs[1].stopSync();
+    motorHatTwo.dcs[2].stopSync();
+    motorHatTwo.dcs[3].stopSync();
+  },
+
+  purge: function () {
+    logger.info('Purging all motors for 10 seconds.');
+    motors.run_all();
+    setInterval(motors.stop_all(), 10000);
+  },
 };
 
 module.exports = motors;
