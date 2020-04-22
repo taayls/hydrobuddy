2b7592081b7d4e9b263d1566e92eec2bd2f2088b
taayls <tayls@protonmail.com>
2020-04-23 05:19:16 +1200

    update to v2 sensors/libraries

--- server/sensors.ino
+++ server/sensors.ino
@@ -12,6 +12,11 @@
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 
+// DFRobot Libraries
+#include "DFRobot_EC.h"
+#include "DFRobot_PH.h"
+#include <EEPROM.h>
+
 // 1 Wire Libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -27,38 +32,31 @@
 const int trigPin = 9;
 const int echoPin = 10;
 
-// EC Constants
-byte ECsensorPin = A1;
-const byte ECnumReadings = 20;
-unsigned int ECAnalogSampleInterval = 25;
-unsigned int ECreadings[ECnumReadings];
-byte index = 0;
-unsigned long ECAnalogValueTotal = 0;
-unsigned int ECAnalogAverage = 0, ECaverageVoltage = 0;
-unsigned long ECAnalogSampleTime;
-float ECcurrent;
-
 // PH Constants
-#define SensorPin A0
-#define Offset 0.00
-#define LED 13
-#define samplingInterval 20
-#define printInterval 800
-#define ArrayLength 40
+#define PH_PIN A0
+DFRobot_PH ph;
+
+// EC Constants
+#define EC_PIN A1
+DFRobot_EC ec;
+
+// EC Variables
+float ec_voltage, ecValue;
+
+// PH Variables
+float ph_voltage, phValue;
 
 // DHT Variables
 int chk;
-float hum;
-float temp;
-float ec_temp;
-
-// PH Variables
-int pHArray[ArrayLength];
-int pHArrayIndex = 0;
+float air_humidity;
+float air_temp;
 
 // Ultrasonic Variables
 long duration;
 int distance;
+
+// Global Variables
+float temperature;
 
 // Setup OneWire Instance
 OneWire oneWire(ONE_WIRE_PIN);
@@ -69,16 +67,12 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup(void)
 {
-  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 
   sensors.begin();
   dht.begin();
-
-  for (byte thisReading = 0; thisReading < ECnumReadings; thisReading++)
-    ECreadings[thisReading] = 0;
-
-  ECAnalogSampleTime = millis();
+  ph.begin();
+  ec.begin();
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -86,64 +80,28 @@ void setup(void)
 
 void loop(void)
 {
-  static unsigned long samplingTime = millis();
-  static unsigned long printTime = millis();
-  static float pHValue, voltage;
-
-  if (millis() - ECAnalogSampleTime >= ECAnalogSampleInterval)
+  static unsigned long timepoint = millis();
+  if(millis()-timepoint>1000U)
   {
-    ECAnalogSampleTime = millis();
-    ECAnalogValueTotal = ECAnalogValueTotal - ECreadings[index];
-    ECreadings[index] = analogRead(ECsensorPin);
-    ECAnalogValueTotal = ECAnalogValueTotal + ECreadings[index];
-    index = index + 1;
-    if (index >= ECnumReadings)
-      index = 0;
-    ECAnalogAverage = ECAnalogValueTotal / ECnumReadings;
-  }
+    timepoint = millis();
+    air_humidity = dht.readHumidity();
+    air_temp = dht.readTemperature();
+    sensors.requestTemperatures();
 
-  if (millis() - samplingTime > samplingInterval)
-  {
-    pHArray[pHArrayIndex++] = analogRead(SensorPin);
-    if (pHArrayIndex == ArrayLength)pHArrayIndex = 0;
-    voltage = averagearray(pHArray, ArrayLength) * 5.0 / 1024;
-    pHValue = 3.5 * voltage + Offset;
-    samplingTime = millis();
-  }
-
-  if (millis() - printTime > printInterval)
-  {
-    // Get Temp/Humidty to output below
-    hum = dht.readHumidity();
-    temp = dht.readTemperature();
-
+    temperature = sensors.getTempCByIndex(0);
 
     Serial.print("Humidity: ");
-    Serial.println(hum);
+    Serial.println(air_humidity);
     Serial.print("Temperature: ");
-    Serial.println(temp);
-    Serial.print("Voltage: ");
-    Serial.println(voltage, 2);
+    Serial.println(air_temp);
     Serial.print("pH: ");
-    Serial.println(pHValue, 2);
-
-    sensors.requestTemperatures();
-    ec_temp = sensors.getTempCByIndex(0);
-
-    Serial.print("EC:");
-    float TempCoefficient = 1.0 + 0.0185 * (ec_temp - 25.0);
-    float CoefficientVoltage = (float)ECaverageVoltage / TempCoefficient;
-    if (CoefficientVoltage < 150)Serial.println("Sensor not in solution!");
-    else if (CoefficientVoltage > 3300)Serial.println("Solution out of range!");
-      else
-      {
-        if (CoefficientVoltage <= 448)ECcurrent = 6.84 * CoefficientVoltage - 64.32;
-          else if (CoefficientVoltage <= 1457)ECcurrent = 6.98 * CoefficientVoltage - 127;
-          else ECcurrent = 5.3 * CoefficientVoltage + 2278;
-          ECcurrent /= 1000;
-          Serial.print(ECcurrent, 2);
-        }
-
+    ph_voltage = analogRead(PH_PIN)/1024.0*5000;
+    phValue = ph.readPH(ph_voltage,temperature);
+    Serial.println(phValue, 2);
+    Serial.print("EC: ");
+    ec_voltage = analogRead(EC_PIN)/1024.0*5000;
+    ecValue =  ec.readEC(ec_voltage,temperature);
+    Serial.print(ecValue,2);
     Serial.print("Temperature Bucket 1: ");
     Serial.println(sensors.getTempCByIndex(0));
     Serial.print("Temperature Bucket 2: ");
@@ -170,47 +128,7 @@ void loop(void)
     Serial.print("Distance: ");
     Serial.println(distance);
 
-    digitalWrite(LED, digitalRead(LED) ^ 1);
-    printTime = millis();
+    ph.calibration(ph_voltage,temperature);
+    ec.calibration(ec_voltage,temperature);
   }
-}
-
-double averagearray(int* arr, int number) {
-  int i;
-  int max, min;
-  double avg;
-  long amount = 0;
-  if (number <= 0) {
-    Serial.println("Error with number for the array!/n");
-    return 0;
-  }
-  if (number < 5) {
-    for (i = 0; i < number; i++) {
-      amount += arr[i];
-    }
-    avg = amount / number;
-    return avg;
-  } else {
-    if (arr[0] < arr[1]) {
-      min = arr[0]; max = arr[1];
-    }
-    else {
-      min = arr[1]; max = arr[0];
-    }
-    for (i = 2; i < number; i++) {
-      if (arr[i] < min) {
-        amount += min;
-        min = arr[i];
-      } else {
-        if (arr[i] > max) {
-          amount += max;
-          max = arr[i];
-        } else {
-          amount += arr[i];
-        }
-      }
-    }
-    avg = (double)amount / (number - 2);
-  }
-  return avg;
 }
